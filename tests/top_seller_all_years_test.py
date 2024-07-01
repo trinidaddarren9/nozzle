aa62aaf15f55572bed63c7efd2d41f2a2e9fd128
Darren <darren@sparkline.com>
2024-07-01 17:46:50 +0800

    added test for /api/v1/sellers/top

--- tests/top_seller_all_years_test.py
+++ tests/top_seller_all_years_test.py
@@ -23,3 +23,28 @@ def test_incorrect_params(client,input_cat,input_ord,expected_key):
     response = client.get(f"/api/v1/sellers/top?{url_params}")
     errors = response.json["errors"]
     assert expected_key in errors
+    
+    
+
+@pytest.mark.parametrize(
+    "input_cat,input_ord,expected_first,expected_last",
+    [
+        ("sales_rep","desc","Steve Johnson","Jane Peacock"),
+        ("sales_rep","asc","Jane Peacock","Steve Johnson"),
+        ("total_sales","desc",221.92,164.34),
+        ("total_sales","asc",164.34,221.92),
+        ("year","desc",2013,2009),
+        ("year","asc",2009,2013),
+    ]
+)
+def test_correct_data(client,input_cat,input_ord,expected_first,expected_last):
+    index = {
+        "sales_rep": "Sales Rep",
+        "total_sales": "Total Sales",
+        "year": "Year"
+    }
+    url_params = urlencode({"category": input_cat, "order": input_ord})
+    response = client.get(f"/api/v1/sellers/top?{url_params}")
+    data = response.json
+    assert data[0][index[input_cat]] == expected_first
+    assert data[-1][index[input_cat]] == expected_last
