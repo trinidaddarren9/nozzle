cb27b27d79531dcabfa7ccd5939b86de66d49c9e
Darren <darren@sparkline.com>
2024-07-01 11:02:55 +0800

    added input validation

--- main/sellers/sellers.py
+++ main/sellers/sellers.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel, field_validator, ValidationError
 from flask_restx import Namespace, Resource
 from main.models import Employee, Customer, Invoice
 
@@ -7,9 +8,24 @@ ns_sellers = Namespace(
     "sellers", description="Contains routes for all seller related data.")
 
 
+class YearValidator(BaseModel):
+    year: int
+
+    @field_validator('year')
+    def check_range(cls, x):
+        if not 1900 <= x <= 2999:
+            raise ValueError("year must be in range of 1900 - 2999")
+
+
 @ns_sellers.route("/<int:year>/top")
 class TopSeller(Resource):
     def get(self, year: int):
+
+        # validate year input
+        try:
+            YearValidator(year=year)
+        except Exception as error:
+            return error.json(), 403
 
         query_result = db.session.query(
             Employee,
@@ -21,6 +37,13 @@ class TopSeller(Resource):
             .group_by(Employee.EmployeeId) \
             .order_by(db.func.sum(Invoice.Total).desc()) \
             .first()
+
+        if query_result is None:
+            return {
+                "status": "error",
+                "message": "No data found."
+            }, 400
+
         return {
             "Sales Rep": f'{query_result[0].__getattribute__("FirstName")} {query_result[0].__getattribute__("LastName") }',
             "Total Sales": float(query_result[1])
